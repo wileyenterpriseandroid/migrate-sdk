b95f1dcc4ab599585bdc7e32a57aa69b3cefd64c
ldornin <ldornin@gmail.com>
2014-02-08 14:17:49 -0500

    Add a browser dist target. Add migrate-browser to migrate binary release.

---
+++ samples/migrate-browser/src/com/migrate/browser/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.migrate.browser;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends Activity
+{
+    /** Will fill this out soon. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
